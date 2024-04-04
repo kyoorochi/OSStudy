6bcdd52b429793d8cf5bf8990cf8a233643be631
kyoorochi <pmh0521@gmail.com>
2024-04-04 14:41:26 +0900

    Mutex Lock Test

---
+++ 03-Day3/05-ThreadTest.py
@@ -0,0 +1,33 @@
+import threading
+from multiprocessing import Process, Value, Lock
+
+def counter1(snum, cnt, lock):
+    lock.acquire()
+    try:
+        for i in range(cnt):
+            snum.value += 1
+    finally:
+        lock.release()
+
+def counter2(snum, cnt, lock):
+    lock.acquire()
+    try:
+        for i in range(cnt):
+            snum.value -= 1
+    finally:
+        lock.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+    shared_number = Value('i', 0)
+
+    t1 = threading.Thread(target=counter1, args=(shared_number, 10000, lock))
+    t1.start()
+
+    t2 = threading.Thread(target=counter2, args=(shared_number, 10000, lock))
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print('Finally, number is', shared_number.value)
