81658cfdf658b26a2498e9b98e19cbefe70f7c9f
kyoorochi <pmh0521@gmail.com>
2024-04-05 11:38:10 +0900

    4일차. 메모리와 페이지 교체 수업. 워킹셋에 대한 정리

---
+++ 04-Day4/02.js
@@ -0,0 +1,15 @@
+let players = {
+    boys : {
+        Beckham : "Midfielder"
+    }
+}
+
+let persons = players
+
+players = ["Son", "Park"]
+
+let human = persons.boys
+
+persons = "persons"
+
+human = null
---
+++ 04-Day4/03.py
@@ -0,0 +1,13 @@
+foods = ['피자', '비빔밥', '파스타']
+
+print(id(foods))
+print(hex(id(foods)))
+
+mv = memoryview(b'Today is Friday')
+
+print(mv)
+
+print(mv[0])
+print(mv[1])
+print(mv[3])
+print(mv[13])
