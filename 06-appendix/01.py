6a5ed89cb8d284ecb4efdf9011734ddafe14fc5a
kyoorochi <pmh0521@gmail.com>
2024-04-09 14:28:45 +0900

    2진법으로 표현한 수를 8진법으로 변환해보기

--- 06-appendix/01.py
+++ 06-appendix/01.py
@@ -8,3 +8,8 @@ print(o_num)
 
 h_num = hex(number) # hex : 주어진 정수를 16진수로 반환한다.
 print(h_num)
+
+# 2진법을 8진법으로 구현시도 int(str(b_num), 2) 에서 , 2는 2진법임을 알려주는것. 8로 하면 8진법을 알려주는 게 된다.
+decimal_number = int(str(b_num), 2)
+oct_num = oct(decimal_number)
+print(oct_num)
